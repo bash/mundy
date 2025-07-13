e1ec2b0620b0aa1426eee48c4e33f4be62081d36
Tau Gärtli <git@tau.garden>
2025-07-13 14:34:09 +0200

    Reduced motion & transparency

--- src/web/bool.rs
+++ src/web/bool.rs
@@ -1,30 +1,59 @@
 use super::drop_on_main_thread::DropOnMainThread;
 use super::event_listener::{EventListenerGuard, EventTargetExt as _};
-use web_sys::{MediaQueryListEvent, Window};
+use web_sys::{MediaQueryList, MediaQueryListEvent, Window};
 
-pub(crate) fn boolean_media_query<T: Copy + 'static>(
-    window: &Window,
-    query: &'static str,
+pub(crate) struct BooleanMediaQuery<'a, T> {
+    window: &'a Window,
+    media_query_list: MediaQueryList,
     truthy: T,
     falsy: T,
-    mut callback: impl FnMut(T) + 'static,
-) -> Option<(DropOnMainThread<EventListenerGuard>, T)> {
-    let media_query_list = window.match_media(query).ok().flatten()?;
+}
 
-    let listener = {
-        move |event: MediaQueryListEvent| {
-            let value = if event.matches() { truthy } else { falsy };
-            callback(value);
+impl<'a, T> BooleanMediaQuery<'a, T>
+where
+    T: 'static + Copy,
+{
+    pub(crate) fn new(
+        window: &'a Window,
+        query: &'static str,
+        truthy: T,
+        falsy: T,
+    ) -> Option<Self> {
+        let media_query_list = window.match_media(query).ok().flatten()?;
+        Some(Self {
+            window,
+            media_query_list,
+            truthy,
+            falsy,
+        })
+    }
+
+    pub(crate) fn value(&self) -> T {
+        if self.media_query_list.matches() {
+            self.truthy
+        } else {
+            self.falsy
         }
-    };
-    let guard = media_query_list
-        .add_event_listener("change", listener)
-        .ok()?;
+    }
 
-    let initial_value = if media_query_list.matches() {
-        truthy
-    } else {
-        falsy
-    };
-    Some((DropOnMainThread::new(guard, window), initial_value))
+    pub(crate) fn subscribe(
+        self,
+        mut callback: impl FnMut(T) + 'static,
+    ) -> Option<DropOnMainThread<EventListenerGuard>> {
+        let listener = {
+            move |event: MediaQueryListEvent| {
+                let value = if event.matches() {
+                    self.truthy
+                } else {
+                    self.falsy
+                };
+                callback(value);
+            }
+        };
+        let guard = self
+            .media_query_list
+            .add_event_listener("change", listener)
+            .ok()?;
+        Some(DropOnMainThread::new(guard, self.window))
+    }
 }

d8fdeb29254d1540d7b8cdbbb086d5127dc7e9c1
Tau Gärtli <git@tau.garden>
2025-08-21 17:39:57 +0200

    Use shlex.join

--- scripts/check-individual-settings.py
+++ scripts/check-individual-settings.py
@@ -2,13 +2,23 @@
 
 from subprocess import check_output, check_call
 import json, sys
+import shlex
 
-metadata = json.loads(check_output(['cargo', 'metadata', '--format-version', '1', '--no-deps']))
-package = next((p for p in metadata['packages'] if p['name'] == 'mundy'))
-preferences = package['features']['_all-preferences']
+metadata = json.loads(
+    check_output(["cargo", "metadata", "--format-version", "1", "--no-deps"])
+)
+package = next((p for p in metadata["packages"] if p["name"] == "mundy"))
+preferences = package["features"]["_all-preferences"]
 
 for pref in preferences:
-    command = ['cargo', 'clippy', '--no-default-features', '--features', f'async-io,{pref}', *sys.argv[1:]]
-    pretty_command = ' '.join(command)
-    print(f'\x1b[1m{pretty_command}\x1b[0m')
+    command = [
+        "cargo",
+        "clippy",
+        "--no-default-features",
+        "--features",
+        f"async-io,{pref}",
+        *sys.argv[1:],
+    ]
+    pretty_command = shlex.join(command)
+    print(f"\x1b[1m{pretty_command}\x1b[0m")
     check_call(command)
